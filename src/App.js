535021f98abc03cd4f97fdc981e5957714ca1c3e
Mikhail Podanev <mpodanev@gmail.com>
2018-02-26 19:49:44 +0200

    components stage 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,171 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import classNames from 'classnames';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+import Input from 'material-ui/Input';
+import Avatar from 'material-ui/Avatar';
+import titleInitials from './utils/title-initials';
+import { chats, messages } from './mock-data';
+import Sidebar from './components/Sidebar';
 
-class App extends Component {
+const styles = theme => ({
+  root: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100vh',
+    backgroundColor: theme.palette.background.default,
+    'overflow-y': 'hidden'
+  },
+  appBar: {
+    position: 'fixed',
+    width: `calc(100% - 320px)`,
+  },
+  drawerPaper: {
+    position: 'relative',
+    height: '100%',
+    width: 320,
+  },
+  drawerHeader: {
+    ...theme.mixins.toolbar,
+    paddingLeft: theme.spacing.unit * 3,
+    paddingRight: theme.spacing.unit * 3,
+  },
+  chatsList: {
+    height: 'calc(100% - 56px)',
+    overflowY: 'scroll',
+  },
+  newChatButton: {
+    position: 'absolute',
+    left: 'auto',
+    right: theme.spacing.unit * 3,
+    bottom: theme.spacing.unit * 3 + 48, // + bottom navigation
+  },
+  chatLayout: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: '64px',
+    height: '100%',
+    overflow: 'hidden',
+  },
+  messagesWrapper: {
+    overflowX: 'scroll',
+    height: '100%',
+    width: '100%',
+    paddingTop: theme.spacing.unit * 3,
+    paddingBottom: '360px',
+  },
+  messageInputWrapper: {
+    position: 'fixed',
+    left: 'auto',
+    right: 0,
+    bottom: 0,
+    width: `calc(100% - 360px)`,
+    padding: theme.spacing.unit * 3,
+  },
+  messageInput: {
+    padding: theme.spacing.unit * 2,
+  },
+  messageWrapper: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 3}px`,
+  },
+  messageWrappperFromMe: {
+    justifyContent: 'flex-end',
+  },
+  message: {
+    maxWidth: '70%',
+    minWidth: '10%',
+    padding: theme.spacing.unit,
+    marginLeft: theme.spacing.unit * 2,
+  },
+  messageFromMe: {
+    marginRight: theme.spacing.unit * 2,
+    backgroundColor: '#e6dcff'
+  },
+});
+
+class App extends React.Component {
+  componentDidMount() {
+    this.scrollDownHistory();
+  }
+
+  componentDidUpdate() {
+    this.scrollDownHistory();
+  }
+
+  scrollDownHistory() {
+    const messagesWrapper = this.refs.messagesWrapper;
+
+    if (messagesWrapper) {
+      messagesWrapper.scrollTop = messagesWrapper.scrollHeight;
+    }
+  }
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <h1>React-chat</h1>
+      <div className={classes.root}>
+        <AppBar color="primary" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="title" color="inherit" noWrap>
+              DogeCodes React Chat
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Sidebar chats="chats"/>
+
+        <main className={classes.chatLayout}>
+          <div className={classes.messagesWrapper} ref="messagesWrapper">
+            {messages && messages.map((message, index) => {
+              const isMessageFromMe = message.sender === 'me';
+
+              const userAvatar = (
+                <Avatar>
+                  {titleInitials(message.sender)}
+                </Avatar>
+              );
+
+              return (
+                // <div className={classNames(classes.messageWrapper, isMessageFromMe && classes.messageWrappperFromMe)}>
+                <div key={index} className={classNames(
+                  classes.messageWrapper,
+                  isMessageFromMe && classes.messageWrappperFromMe
+                )}>
+                  {!isMessageFromMe && userAvatar}
+                  <Paper className={classNames(
+                    classes.message,
+                    isMessageFromMe && classes.messageFromMe
+                  )}>
+                    <Typography variant="caption">
+                      {message.sender}
+                    </Typography>
+                    <Typography variant="body1">
+                      {message.content}
+                    </Typography>
+                  </Paper>
+                  {isMessageFromMe && userAvatar}
+                </div>
+              );
+            })}
+          </div>
+          <div className={classes.messageInputWrapper}>
+            <Paper className={classes.messageInput} elevation={6}>
+              <Input fullWidth placeholder="Type your message…"/>
+            </Paper>
+          </div>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
